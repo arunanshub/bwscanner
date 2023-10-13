4a1f5178aeab60d0d72f6f95938b0a3d60b30fcc
Arunanshu Biswas <mydellpc07@gmail.com>
2023-10-13 21:18:37 +0530

    chore: move functions to respective modules

---
+++ src/bwscan/builtwith.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import typing
+from dataclasses import dataclass
+
+from lxml import html
+
+from . import net, utils
+
+if typing.TYPE_CHECKING:
+    import aiohttp
+
+
+@dataclass
+class TechnologyDetails:
+    description: str
+    site: str
+    tags: str
+    image_link: str
+
+
+def get_client_websites(source: str) -> list[str]:
+    """
+    Parse the "Uses" page of BuiltWith and list the websites that uses a
+    particular technology.
+    """
+    urls = html.fromstring(source).xpath(
+        r".//tr[@data-domain]/td[@class='pl-0 text-primary']/text()"
+    )
+    assert isinstance(urls, list)
+    return list(map(utils.get_normalized_url, urls))  # type: ignore
+
+
+def get_technology_link(source: str) -> str | None:
+    """
+    Parse the BuiltWith "Uses" page and get the link to the BuiltWith
+    "Technology Overview" page.
+    """
+    tree = html.fromstring(source)
+    technology_link_arr = tree.xpath(
+        r".//nav[@aria-label='breadcrumb']/ol/li/a/@href[contains(., '//trends')]"  # noqa: E501
+    )
+    assert isinstance(technology_link_arr, list)
+    if not len(technology_link_arr):
+        return None
+    return utils.get_normalized_url(technology_link_arr[0])  # type: ignore
+
+
+async def get_technology_details(
+    session: aiohttp.ClientSession,
+    technology_site: str,
+) -> TechnologyDetails | None:
+    """
+    Get the technology details from the BuiltWith "Technology Details" page.
+    """
+    site_data = await net.get_response(session, technology_site)
+    if site_data is None:
+        return None
+
+    tree = html.fromstring(site_data.text)
+    p_elements = tree.xpath(r".//div/*/div[@class='col-9 col-md-10']/p")
+    assert isinstance(p_elements, list)
+    if len(p_elements) != 3:  # noqa: PLR2004
+        return None
+
+    # get image link
+    image_el = tree.xpath(
+        r".//div[@class='col-md-2 col-3 text-center']/img/@data-src"
+    )
+    assert isinstance(image_el, list)
+    image_link = typing.cast(str, image_el.pop())
+
+    # TODO: the type errors are too complex to solve. Python tries to be
+    #  TypeScript without giving TypeScript's benefits.
+    p_description, p_techsite, p_tags = p_elements
+    description = p_description.text  # type: ignore
+    technology_site = p_techsite.xpath(r".//a/text()")[0]  # type: ignore
+    tags = p_tags.xpath(r".//a/text()")  # type: ignore
+    return TechnologyDetails(description, technology_site, tags, image_link)  # type: ignore
---
+++ src/bwscan/utils.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import urllib.parse
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import re
+
+
+def get_normalized_url(url: str) -> str:
+    """
+    Takes a url of form `//site.com` or `site.com` and converts it to `https://site.com`.
+    """
+    parsed_url = urllib.parse.urlparse(url, "https")
+    normalized_url = parsed_url
+    if not parsed_url.netloc and parsed_url.path:
+        normalized_url = parsed_url._replace(netloc=parsed_url.path, path="")
+    return urllib.parse.urlunparse(normalized_url)
+
+
+def is_regex_matching(data: str, regex: re.Pattern[str]) -> bool:
+    """Check if regex matches or not."""
+    return regex.search(data) is not None
